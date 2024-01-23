419db43a30434f8c688c740773e2ed9a45c168c3
pauloRzeszut <agustinrzeszut@hotmail.com>
2024-01-23 17:42:08 -0300

    trying to bugfix

--- src/repository/carts.repository.js
+++ src/repository/carts.repository.js
@@ -3,24 +3,13 @@ const url = import.meta.env.VITE_BACKEND_URL;
 
 export const getCartRepository = async () => {
     try {
-        const response = await fetch(`${url}/api/carts`, {
-            credentials: 'include',  // Include cookies for cross-origin requests
-          });
-      
-          if (!response.ok) {
-            throw new Error(`HTTP error ${response.status}`);
-          }
-      
-          const data = await response.json();  // Assuming a JSON response
-          console.log(data)
-          return data;
-        // const res = await axios.get(
-        //     `${url}/api/carts`, 
-        //     {
-        //         withCredentials: true
-        //     }
-        // );
-        // return res;
+        const res = await axios.get(
+            `${url}/api/carts`, 
+            {
+                withCredentials: true
+            }
+        );
+        return res;
     } catch (e) {
         throw e;
     };
